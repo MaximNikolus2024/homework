6c9e9c1481d5a15d590c096cc653a6e8679e22ee
Maxim Nikolus <nikolusmaxim409@gmail.com>
2024-04-24 17:42:54 +0200

    update Student

--- src/lesson18/homework/Student.java
+++ src/lesson18/homework/Student.java
@@ -12,6 +12,22 @@ public class Student {
         this.age = age;
     }
 
+    public String getName(){
+        return name;
+    }
+
+    public void setName (String name){
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+public void setAge(int age){
+        if (age >= 0){
+            this.age = age;
+        }
+}
+
     public void sleep() {
         System.out.println("Я сплю");
         System.out.println("Дай ещё 5 минут.");
